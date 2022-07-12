eb594f4fcc558558dabe13890fec93509b7488d5
Andhy Ralac <andhyralac11@gmail.com>
2022-07-12 01:01:56 -0600

    specific error handling function

--- src/api/utils/handleError.ts
+++ src/api/utils/handleError.ts
@@ -5,3 +5,17 @@ export const handleHttpError = (res:Response, error:any) => {
     res.status(500);
     res.send({ error: "SERVER_ERROR"})
 }
+
+
+
+/**
+ * Handle error specify
+ * @param res 
+ * @param message 
+ * @param code 
+ */
+export const handleErrorResponse = (res:Response, message:string, code:number) => {
+    console.log('Error: ', message);
+    res.status(code)
+    res.send({ error: message })
+}
