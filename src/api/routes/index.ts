1191feacdf060dde126ed3abd2b0f824e22d2803
Andhy Ralac <andhyralac11@gmail.com>
2022-07-18 00:36:33 -0600

    authentication route was added in the route index.ts file

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -4,9 +4,11 @@ const routes:Router = Router()
 import routesUser from './user.routes'
 import routesPhoto from './photo.routes'
 import routesComment from './comment.routes'
+import routesAuth from './auth.routes'
 
 routes.use('/users', routesUser)
 routes.use('/photos', routesPhoto)
 routes.use('/comments', routesComment)
+routes.use('/auth', routesAuth)
 
 export default routes
