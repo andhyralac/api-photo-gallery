bd0de3a37511bb0c4ba37cc2b92938f7a6a2a4ec
Andhy Ralac <andhyralac11@gmail.com>
2022-07-21 23:38:25 -0600

    added jswon web token environment variables, allowed image extensions, cloudinary

--- src/config/default.ts
+++ src/config/default.ts
@@ -2,5 +2,7 @@ export default {
     PORT: process.env.PORT,
     DEVELOPMENT: process.env.DEVELOPMENT || '',
     MONGO_URI: process.env.MONGO_URI || '',
-    JWT_SECRET: process.env.JWT_SECRET || ''
+    JWT_SECRET: process.env.JWT_SECRET || '',
+    ALLOWED_IMAGE_EXTENSIONS: ['jpg', 'jpeg', 'png', 'gif'],
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL || ''
 }
